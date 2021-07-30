e2a123019c3f9e6ba27f43b97a8852fe6d70a3de
rishabh1208 <rishabhsinghal1215@gmail.com>
2021-07-30 23:20:58 +0530

    Final commit

--- middleware/error.js
+++ middleware/error.js
@@ -1,32 +1,35 @@
-const ErrorResponse = require("../utils/errorResponse");
+const ErrorResponse = require('../utils/errorResponse');
 
-function errorHandler(err, req, res, next) {
+const errorHandler = (err, req, res, next) => {
   let error = { ...err };
+
   error.message = err.message;
-  // console.log(err);
 
-  //Mongoose Duplicate Key
+  // Log to console for dev
+  console.log(err);
+
+  // Mongoose bad ObjectId
+  if (err.name === 'CastError') {
+    const message = `Resource not found`;
+    error = new ErrorResponse(message, 404);
+  }
+
+  // Mongoose duplicate key
   if (err.code === 11000) {
-    const message = "Duplicate field value entered";
-    error = new ErrorResponse(message, 404);
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
   }
 
-  //Mongoose Validation error
-  if (err.name === "ValidationError") {
-    const message = Object.values(err.errors).map((val) => val.message);
-    error = new ErrorResponse(message, 404);
-  }
-
-  //   Mongoose bad objectID
-  if (err.name === "CastError") {
-    const message = `Bootcamp not found with id of ${err.value}`;
-    error = new ErrorResponse(message, 404);
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
   }
 
   res.status(error.statusCode || 500).json({
     success: false,
-    error: error.message || "server error",
+    error: error.message || 'Server Error'
   });
-}
+};
 
 module.exports = errorHandler;
